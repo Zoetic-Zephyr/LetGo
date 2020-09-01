b695b643c4d6436c945a2ba34d80af12ad484189
Zoetic-Zephyr <jack.zhang@nyu.edu>
2020-09-01 19:08:21 -0400

    Complete authentication feature with Amplify

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { Authenticator } from "aws-amplify-react-native";
+
+Amplify.configure(awsconfig);
+
+console.disableYellowBox = true;
+
+function Home(props) {
+  if (props.authState === "signedIn") return <Text>HOME</Text>;
+  else return <></>;
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HOME</Text>
+      <Authenticator usernameAttributes="email">
+        <Home />
+      </Authenticator>
     </View>
   );
 }
